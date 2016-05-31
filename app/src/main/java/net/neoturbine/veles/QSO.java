f6cd35eba4117d10b2a8bca30487ff893c8780aa
Joseph Jon Booker <joe@neoturbine.net>
2016-05-31 13:05:10 -0400

    Show power in DetailsFragment

--- app/src/main/java/net/neoturbine/veles/QSO.java
+++ app/src/main/java/net/neoturbine/veles/QSO.java
@@ -13,19 +13,22 @@ public final class QSO {
     @NotNull
     private final String mMode;
     @NotNull
+    private final String mPower;
+    @NotNull
     private final String mTxFrequency;
     @NotNull
     private final String mRxFrequency;
     @NotNull
     private final String mComment;
-
     private QSO(@NotNull String otherStation,
-                long startTime, long endTime, @NotNull String mode, @NotNull String txFrequency,
+                long startTime, long endTime, @NotNull String mode, @NotNull String power,
+                @NotNull String txFrequency,
                 @NotNull String rxFrequency, @NotNull String comment) {
         this.mOtherStation = otherStation;
         this.mStartTime = startTime;
         this.mEndTime = endTime;
         this.mMode = mode;
+        this.mPower = power;
         this.mTxFrequency = txFrequency;
         this.mRxFrequency = rxFrequency;
         this.mComment = comment;
@@ -37,10 +40,15 @@ public final class QSO {
                 data.getLong(data.getColumnIndexOrThrow(QSOColumns.START_TIME)),
                 data.getLong(data.getColumnIndexOrThrow(QSOColumns.END_TIME)),
                 data.getString(data.getColumnIndexOrThrow(QSOColumns.MODE)),
+                data.getString(data.getColumnIndexOrThrow(QSOColumns.POWER)),
                 data.getString(data.getColumnIndexOrThrow(QSOColumns.TRANSMISSION_FREQUENCY)),
                 data.getString(data.getColumnIndexOrThrow(QSOColumns.RECEIVE_FREQUENCY)),
                 data.getString(data.getColumnIndexOrThrow(QSOColumns.COMMENT))
         );
+    }
+
+    public String getPower() {
+        return mPower;
     }
 
     public String getOtherStation() {
