d96ec0d46a551c545fa9f981d1698136638f796a
Joseph Jon Booker <joe@neoturbine.net>
2017-04-21 14:25:37 -0400

    Implement LeakCanery into the debug builds

--- app/src/main/java/net/neoturbine/veles/VelesApplication.java
+++ app/src/main/java/net/neoturbine/veles/VelesApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import android.app.Fragment;
 import android.support.annotation.NonNull;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import net.neoturbine.veles.qso.data.ApplicationContextModule;
 
 import javax.inject.Inject;
@@ -23,6 +25,11 @@ public class VelesApplication extends Application implements HasDispatchingActiv
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+
         getComponent().inject(this);
     }
 
